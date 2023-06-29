79f3aa703163d10b90e4f5a81b079849eeef3dc6
wangmingyao <wmyaoo@outlook.com>
2023-06-29 10:15:48 +0800

    feat[tag]新增博客的标签功能CRUD

---
+++ routers/router.go
@@ -0,0 +1,38 @@
+package routers
+
+import (
+	v1 "github.com/Walk2future/w-w-go-gin-example/routers/api/v1"
+	"github.com/gin-gonic/gin"
+
+	"github.com/Walk2future/w-w-go-gin-example/pkg/setting"
+)
+
+func InitRouter() *gin.Engine {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+
+	r.Use(gin.Recovery())
+
+	gin.SetMode(setting.RunMode)
+
+	//r.GET("/test", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "test",
+	//	})
+	//})
+
+	apiv1 := r.Group("/api/v1")
+	{
+
+		apiv1.GET("/tags", v1.GetTags)
+
+		apiv1.POST("/tags", v1.AddTags)
+
+		apiv1.PUT("/tags/:id", v1.EditTags)
+
+		apiv1.DELETE("/tags/:id", v1.DeleteTags)
+	}
+
+	return r
+}
